2c6f53d6e748970768948b53c89ef1e3725269c6
BANO.notIT <bano.notit@yandex.ru>
2018-07-08 20:51:51 +0300

    add tests

---
+++ __tests__/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by BANO.notIT on 08.07.18.
+ */
+
+const
+    fabric = require('../dist/index.esm').default,
+    config = require('./config')
+
+
+test('fabric if function', () => {
+    expect(fabric).toBeInstanceOf(Function)
+})
+
+test('fabric returns function', () => {
+    expect(fabric({config})).toBeInstanceOf(Function)
+})
